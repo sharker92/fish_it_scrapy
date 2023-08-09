7bf7fc36ef7d37a8ce0153f08b34a70a6636792f
Andrés Morales Reyes <sharker92@hotmail.com>
2023-08-09 16:11:40 -0600

    👌 IMPROVE: Add scraper into product url.

--- fish_it/fish_it/spiders/alsuper.py
+++ fish_it/fish_it/spiders/alsuper.py
@@ -5,12 +5,11 @@ from scrapy.loader import ItemLoader
 
 # https://api2.alsuper.com/v1/ms-products/branch/1000?page=5&limit=5000
 # jala todos los productos y da más info. Es como la base de datos principal.
-# https://api2.alsuper.com/v1/products/1152/branch/2 El branch no tiene sentido?!
-# normalmente usan el 6 -> del 1 al 9 y la 1000
+# https://api2.alsuper.com/v1/products/1152/branch/2
+# la branch es el supermercado. Leones es el 6, el 1000 es el generico.
 # sirve para obtener la info de un producto en especifico
-# la branch es el super. Leones es el 6.
 
-PRODUCTS_PER_PAGE = 5000
+PRODUCTS_PER_PAGE = 1
 
 
 class AlsuperSpider(scrapy.Spider):
@@ -25,17 +24,15 @@ class AlsuperSpider(scrapy.Spider):
             products = response.json()["data"]["data"]
             for p in products:
                 il = ItemLoader(item=AlsuperItem(), selector=p)
-                il.add_value("id", p.get("id", None))
-                il.add_value("name", p.get("name", None))
-                il.add_value("price", p.get("price", None))
-                il.add_value("image_url", p.get("image_url", None))
-                il.add_value("unit", p.get("unit", None))
-                il.add_value("weight", p.get("weight", None))
-                il.add_value("packing", p.get("packing", None))
-                il.add_value("variant", p.get("variant", None))
-                il.add_value("madurity", p.get("madurity", None))
-                il.add_value("share_url", p.get("share_url", None))
-                yield il.load_item()
+                il.add_value("ecommerce", p.get("ecommerce", None))
+                product_id = p.get("id", None)
+                product_url = (
+                    f"https://api2.alsuper.com/v1/products/{product_id}/branch/1000"
+                )
+                yield response.follow(
+                    product_url, meta={"il": il}, callback=self.parse_product_page
+                )
+
             # yield from products
 
             page_number = re.search(r"(?<=page=)\d+", response.url).group()
@@ -43,7 +40,23 @@ class AlsuperSpider(scrapy.Spider):
             relative_url = f"1000?page={next_page_number}&limit={PRODUCTS_PER_PAGE}"
             yield response.follow(relative_url, callback=self.parse)
         except KeyError:
-            print('No se encontraron más articulos.')
+            print("No se encontraron más articulos.")
+
+    def parse_product_page(self, response):
+        product = response.json()["data"]
+        il = response.meta["il"]
+        il.add_value("id", product.get("id", None))
+        il.add_value("name", product.get("name", None))
+        il.add_value("price", product.get("price", None))
+        il.add_value("image_url", product.get("image_url", None))
+        il.add_value("unit", product.get("unit", None))
+        il.add_value("weight", product.get("weight", None))
+        il.add_value("packing", product.get("packing", None))
+        il.add_value("variant", product.get("variant", None))
+        il.add_value("madurity", product.get("madurity", None))
+        il.add_value("share_url", product.get("share_url", None))
+        il.add_value("ean", product.get("ean", None))
+        yield il.load_item()
 
 
 # {
--- fish_it_scrapy/main.py
+++ fish_it_scrapy/main.py
@@ -2,18 +2,21 @@ import json
 
 
 def main():
-    with open("2023-08-05-full_alsuper.json", "r") as f:
+
+    # with open("ms-products-alsuper-2023-08-08.json", "r") as f:
+    with open("products-alsuper-2023-08-08.json", "r") as f:
         data = json.loads(f.read())
         keys = set()
         num = 0
         for d in data:
             num += 1
-            for d in d.keys():
+            # for d in d.keys():
+            for d in d['data'].keys():
                 keys.add(d)
         with open("keys.txt", "w") as file:
             file.write(str(keys))
         print(num)
-        print(str(keys))
+        print(str(sorted(keys)))
 
 
 if __name__ == "__main__":
